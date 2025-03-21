c5151d3791c261872e6b6c5f667041ed4c9d77dd
origami0352 <kyn008116@gamil.com>
2025-03-21 17:30:51 +0900

    feat: fix register and login API, add logout and refresh API

--- app/src/main/java/com/project/feelrobot/network/Api.kt
+++ app/src/main/java/com/project/feelrobot/network/Api.kt
@@ -1,16 +1,24 @@
 package com.project.feelrobot.network
 
 import com.project.feelrobot.model.dto.LoginRequestDto
+import com.project.feelrobot.model.dto.LoginResponseDto
+import com.project.feelrobot.model.dto.LogoutRequestDto
+import com.project.feelrobot.model.dto.RefreshDto
 import com.project.feelrobot.model.dto.RegisterDto
-import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface Api {
     @POST("/sign/register")
-    suspend fun register(@Body registerDto: RegisterDto): Response<ResponseBody>
+    suspend fun register(@Body registerDto: RegisterDto):Response<String>
 
     @POST("/sign/login")
-    suspend fun login(@Body loginRequestDto: LoginRequestDto): Response<ResponseBody>
+    suspend fun login(@Body loginRequestDto: LoginRequestDto): Response<LoginResponseDto>
+
+    @POST("/sign/logout")
+    suspend fun logout(@Body logoutRequest: LogoutRequestDto): Response<String>
+
+    @POST("/sign/refresh")
+    suspend fun refreshToken(@Body refreshDto: RefreshDto): Response<LoginResponseDto>
 }
