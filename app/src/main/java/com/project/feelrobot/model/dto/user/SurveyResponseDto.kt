5409cd9ebe3cd72bdb0266edd09673bc18f38835
origami0352 <kyn008116@gamil.com>
2025-04-02 01:09:32 +0900

    fix: fix data type of sex

--- app/src/main/java/com/project/feelrobot/model/dto/user/SurveyResponseDto.kt
+++ app/src/main/java/com/project/feelrobot/model/dto/user/SurveyResponseDto.kt
@@ -3,6 +3,6 @@ package com.project.feelrobot.model.dto.user
 data class SurveyResponseDto (
     val userId: String,
     val birth: String,
-    val sex: String,
+    val sex: Int,
     val managerId: String
 )
