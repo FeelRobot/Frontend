de3026f9d56043168cb44be24dd628514b3b6137
origami0352 <kyn008116@gamil.com>
2025-04-06 20:39:47 +0900

    feat: rename sidebar button

--- app/src/main/java/com/project/feelrobot/components/Sidebar.kt
+++ app/src/main/java/com/project/feelrobot/components/Sidebar.kt
@@ -86,7 +86,7 @@ fun Sidebar(navController: NavController, drawerState: DrawerState) {
             SidebarButton("홈", navController, "home", drawerState, scope) {
                 Icon(Icons.Default.Home, contentDescription = "Home Icon") // 아이콘 추가
             }
-            SidebarButton("내 정보", navController, "myPage", drawerState, scope) {
+            SidebarButton("마이페이지", navController, "myPage", drawerState, scope) {
                 Icon(Icons.Default.Person, contentDescription = "My page")
             }
             SidebarButton("회원가입", navController, "signup", drawerState, scope) {
