e08baf62b9ccf1c5e38c4dd82bd1ec6d1774194c
origami0352 <kyn008116@gamil.com>
2025-04-10 11:05:54 +0900

    fix: remove Bearer when sending token

--- app/src/main/java/com/project/feelrobot/network/JwtInterceptor.kt
+++ app/src/main/java/com/project/feelrobot/network/JwtInterceptor.kt
@@ -2,6 +2,7 @@ package com.project.feelrobot.network
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import com.project.feelrobot.storage.JwtTokenManager
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
@@ -18,10 +19,11 @@ class JwtInterceptor(private val context: Context) : Interceptor {
                 null
             }
         }
+        Log.d("JwtInterceptor", "추가할 토큰: $token")
 
         val request = chain.request().newBuilder()
         token?.let {
-            request.addHeader("Authorization", "Bearer $it") // JWT 포함
+            request.addHeader("Authorization", it) // JWT 포함, Bearer 제거
         }
 
         return chain.proceed(request.build())
